7f1862ed8a224aa380e87fbedac5236de8763fcc
miategeris <miategeris@gmail.com>
2021-09-06 21:46:53 -0400

    Printed hello world/Turtle practice

---
+++ turtle practice.py
@@ -0,0 +1,19 @@
+# turtle_practice.py
+# by Mia Tegeris
+# September 6, 2021
+# Uses the turtle module to create basic shapes
+
+#Imports necessary turtle commands
+import turtle
+
+#The following lines make a square
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+
+#This command keeps the window from closing immediately
+turtle.exitonclick()
